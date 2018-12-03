7b77fbecfdc1810fbec430cdac5133aa5fd07f0f
mai1121 <maiukawa@maiukawanoMacBook-ea.local>
2018-12-03 15:20:32 +0900

    Edit controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,7 @@
 class GroupsController < ApplicationController
+
+  before_action :set_group, only: [:edit, :update]
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -19,12 +22,20 @@ class GroupsController < ApplicationController
   end
 
   def update
-
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 
 
