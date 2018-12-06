5e991ff36eee5ca29074f0c0df8723db86cf75cf
mai1121 <maiukawa@maiukawanoMacBook-ea.local>
2018-12-06 12:09:39 +0900

    Edit factory file

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,6 +1,7 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :message do
     content Faker::Lorem.sentence
+    # 該当のフォルダに適当な画像をおいてやる
     image File.open("#{Rails.root}/public/images/no_image.jpg")
     user
     group
